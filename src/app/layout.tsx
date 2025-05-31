d63fa86949dce8d9734214fb031c0bd1aeffb540
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-05-31 11:25:11 +0100

    🚀 feat: Remove AuthGuard from RootLayout to simplify component structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         <Providers>
-          <AuthGuard>
             {children}
-          </AuthGuard>
         </Providers>
       </body>
     </html>
