9dcfd5940cec87a442f5f00693033220383030c1
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-10-03 19:55:50 +0000

    remove unnecessary packages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Ignore optional keyv adapters that aren't used
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+    };
+    
+    config.externals = [
+      ...(config.externals || []),
+      {
+        '@keyv/redis': 'commonjs @keyv/redis',
+        '@keyv/mongo': 'commonjs @keyv/mongo',
+        '@keyv/sqlite': 'commonjs @keyv/sqlite',
+        '@keyv/postgres': 'commonjs @keyv/postgres',
+        '@keyv/mysql': 'commonjs @keyv/mysql',
+        '@keyv/etcd': 'commonjs @keyv/etcd',
+        '@keyv/offline': 'commonjs @keyv/offline',
+        '@keyv/tiered': 'commonjs @keyv/tiered',
+      },
+    ];
+    
+    // Ignore dynamic require warnings for optional keyv adapters
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      {
+        module: /node_modules\/keyv/,
+        message: /Can't resolve '@keyv\//,
+      },
+      {
+        module: /node_modules\/cacheable-request/,
+      },
+    ];
+
+    return config;
   },
 };
 
